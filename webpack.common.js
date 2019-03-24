ea309d7b2eac8d321ae53c5933a30a1ca301ed67
iamdeveloper <danlex@localhost.localdomain>
2019-03-24 21:21:26 +0100

    style fixes

--- webpack.common.js
+++ webpack.common.js
@@ -3,31 +3,29 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    },
     plugins: [new HtmlWebpackPlugin({
         template: './src/template.html',
     })],
-    rules: [
-        {
-            test: /\.scss$/,
-            use:[
-                "style-loader", // 3. Injects styles into DOM
-                "css-loader", // 2. Turns css into js
-                "sass-loader" // 1. Turns sass into css
-                ]
-        },
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader",
-                options: {
-                    presets: ['@babel/preset-env']
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use:[
+                    "style-loader", // 3. Injects styles into DOM
+                    "css-loader", // 2. Turns css into js
+                    "sass-loader" // 1. Turns sass into css
+                    ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
                 }
             }
-        }
-    ]
+        ]
+    }
 };

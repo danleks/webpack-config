188df4b205154cefbf7b226ff3342e2bcdf859b6
iamdeveloper <danlex@localhost.localdomain>
2019-03-24 20:32:33 +0100

    add babel and browserslist

--- webpack.common.js
+++ webpack.common.js
@@ -18,6 +18,16 @@ module.exports = {
                 "css-loader", // 2. Turns css into js
                 "sass-loader" // 1. Turns sass into css
                 ]
+        },
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
         }
     ]
 };
